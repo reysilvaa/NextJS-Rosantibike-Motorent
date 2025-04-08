c7992cc8d7345c42ddf19251515881d1967d07d7
reysilvaa <reynaldsilva123@gmail.com>
2025-04-08 14:46:47 +0700

    Add reactStrictMode to configuration and enhance CORS headers
    Updated the Next.js configuration to enable reactStrictMode for improved performance and debugging. Enhanced CORS headers to allow all origins and specified allowed headers for better API interaction across routes.

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,7 @@ try {
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -22,6 +23,7 @@ const nextConfig = {
         pathname: '/**',
       },
     ],
+    domains: ['*'],
   },
   experimental: {
     webpackBuildWorker: true,
@@ -64,12 +66,25 @@ const nextConfig = {
   async headers() {
     return [
       {
+        // Apply these headers to all routes
         source: '/:path*',
         headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization' },
+          {
+            key: 'Access-Control-Allow-Credentials',
+            value: 'true',
+          },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: '*',
+          },
         ],
       },
     ];
