2b86aa7bfd0814cfbced37f666e48295ee9532e6
reysilvaa <reynaldsilva123@gmail.com>
2025-04-17 01:47:22 +0700

    pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,8 @@ export default {
   apps: [
     {
       name: 'rental-frontend',
-      script: 'npm',
-      args: 'run start',
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start -p 3001',
       cwd: './',
       instances: 'max',
       exec_mode: 'cluster',
@@ -16,4 +16,4 @@ export default {
       },
     },
   ],
-}; 
+};
