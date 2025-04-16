381bc28c56c0c0802adce208b871dcfd76d55f26
reysilvaa <reynaldsilva123@gmail.com>
2025-04-17 01:54:00 +0700

    Perbarui konfigurasi PM2 untuk frontend dengan mengubah format dari ESM ke CommonJS.

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,4 +1,4 @@
-// Konfigurasi PM2 untuk frontend - format CommonJS
+// Konfigurasi PM2 untuk frontend
 module.exports = {
   apps: [
     {
@@ -16,4 +16,4 @@ module.exports = {
       },
     },
   ],
-};
+}; 
